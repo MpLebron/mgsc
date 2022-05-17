9f05c7ec7ed8b7689d90022b87e2ae9bfa1bd0a2
MpLebron <llonggis@163.com>
2022-05-17 15:04:10 +0800

    完成到发送login请求

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
+var webpack = require('webpack');
 module.exports = defineConfig({
   transpileDependencies: true,
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "windows.jQuery": "jquery"
-    })
-  ],
-  resolve: {
-    alias: {
-      jquery: "jquery/src/jquery",
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "windows.jQuery": "jquery"
+      })
+    ],
+    //chunks: ['manifest', 'vendor', 'app'],
+    resolve: {
+      alias: {
+        jquery: "jquery/src/jquery",
+      }
+    }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://www.igu-geomodeling.com/',
+        pathRewrite: { '^/api': '' },
+        changeOrigin: true
+      }
     }
   }
 })
